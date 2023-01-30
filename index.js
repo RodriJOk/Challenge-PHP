ac33ca8b1c1bfbf46f17760ad9dee1eeb83ff3ed
RodriJBidcom <rodrigoj@bidcom.com.ar>
2023-01-30 18:20:14 -0300

    Add challenge of code

---
+++ index.js
@@ -0,0 +1,3 @@
+let rodri = [0,1,2]
+
+console.log(rodri.indexOf(3)) // 1
---
+++ index.php
@@ -0,0 +1,26 @@
+<!-- Pregunta 1
+Escribir un programa utilizando PHP que encuentre los dos 
+elementos del arreglo que sumados dan 10. 
+Se deben imprimir ambos números como resultado separados por un espacio 
+(en el orden en que aparecen en el arreglo).
+Por ejemplo, para el arreglo (1,3,4,2,7,0) el resultado sería: 3 7 -->
+<?php
+
+function findSum($arr) {
+    $result = [];
+    for ($i = 0; $i < count($arr); $i++) {
+        for ($j = 0; $j < count($arr); $j++) {
+            if ($arr[$i] + $arr[$j] == 10) {
+                $result[] = $arr[$i];
+                $result[] = $arr[$j];
+                return $result;
+            }
+        }
+    }
+    return $result;
+}
+
+print_r(findSum([1,3,4,2,7,0])); // 3 7
+print_r(findSum([1,5,4,2,7,0])); // 5 5
+
+?>
